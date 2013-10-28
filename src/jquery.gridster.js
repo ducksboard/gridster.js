d4ec7140f736bc30697c75b54ed3242ddf1d75b9
activeworx <activeworx@gmail.com>
2013-10-28 10:04:21 -0400

    feat(resize): Add start/stop/resize callbacks

--- src/jquery.gridster.js
+++ src/jquery.gridster.js
@@ -100,6 +100,12 @@
     *       @param {Array} [options.resize.max_size] Limit widget dimensions
     *        when resizing. Array values should be integers:
     *        `[max_cols_occupied, max_rows_occupied]`
+    *       @param {Function} [options.resize.start] Function executed
+    *        when resizing starts.
+    *       @param {Function} [otions.resize.resize] Function executed
+    *        during the resizing.
+    *       @param {Function} [options.resize.stop] Function executed
+    *        when resizing stops.
     *
     * @constructor
     */
@@ -1043,6 +1049,10 @@
         }).appendTo(this.$el);
 
         this.$resized_widget.addClass('resizing');
+
+		if (this.options.resize.start) {
+            this.options.resize.start.call(this, event, ui, this.$resized_widget);
+        }
     };
 
 
@@ -1069,6 +1079,10 @@
                     'min-height': ''
                 });
         }, this), 300);
+
+        if (this.options.resize.stop) {
+            this.options.resize.stop.call(this, event, ui, this.$resized_widget);
+        }
     };
 
     /**
@@ -1133,6 +1147,10 @@
                 'data-sizex': size_x,
                 'data-sizey': size_y
             });
+        }
+
+        if (this.options.resize.resize) {
+            this.options.resize.resize.call(this, event, ui, this.$resized_widget);
         }
 
         this.resize_last_sizex = size_x;
