6f207230393f068bcab13f21b547c708f9fc3983
Martin Woodward <martinwo@microsoft.com>
2014-01-09 22:43:28 -0800

    fix(licenses): add required copyright message for underscore
    Add the copyright message for the code taken from underscore as required
    by the MIT license.

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,37 @@
 ;(function(window, undefined) {
 
+    /* Delay, debounce and throttle functions taken from underscore.js
+     *
+     * Copyright (c) 2009-2013 Jeremy Ashkenas, DocumentCloud and
+     * Investigative Reporters & Editors
+     *
+     * Permission is hereby granted, free of charge, to any person
+     * obtaining a copy of this software and associated documentation
+     * files (the "Software"), to deal in the Software without
+     * restriction, including without limitation the rights to use,
+     * copy, modify, merge, publish, distribute, sublicense, and/or sell
+     * copies of the Software, and to permit persons to whom the
+     * Software is furnished to do so, subject to the following
+     * conditions:
+     *
+     * The above copyright notice and this permission notice shall be
+     * included in all copies or substantial portions of the Software.
+     *
+     * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+     * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+     * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+     * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+     * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+     * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+     * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+     * OTHER DEALINGS IN THE SOFTWARE.
+     */
 
     window.delay = function(func, wait) {
         var args = Array.prototype.slice.call(arguments, 2);
         return setTimeout(function(){ return func.apply(null, args); }, wait);
     };
 
-
-    /* Debounce and throttle functions taken from underscore.js */
     window.debounce = function(func, wait, immediate) {
         var timeout;
         return function() {
@@ -21,7 +45,6 @@
           timeout = setTimeout(later, wait);
         };
     };
-
 
     window.throttle = function(func, wait) {
         var context, args, timeout, throttling, more, result;
